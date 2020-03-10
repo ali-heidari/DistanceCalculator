c11ca4735e9dccc937061a6a6d1fce1c6f0bf507
ali <ali-heidari@outlook.com>
2020-03-10 21:35:36 +0330

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DistanceCalculator
-A simple web application written in .Net Core 
+
+A simple web application written in .Net Core
+
+## The units
+
+1. WebAPP
+    - Dotnet core MVC web application
+2. WebAPI
+    - Dotnet core API
+3. GeoService
+    - Service to calculate distance between 2 geo-points
+4. Core
+    - Core of system. Currently Database layer is only component.
+
+## What is used
+
+1. NServiceBus
+2. RabbitMQ
+3. DatabaseLayer
+4. WebAPI
+5. Swagger
+6. Collection of postman 
--- WebAPI/Controllers/GeoController.cs
+++ WebAPI/Controllers/GeoController.cs
@@ -18,22 +18,33 @@ namespace WebAPI.Controllers
             this._manager = manager;
         }
 
+        /// <summary>
+        /// Get distance between 2 geo points
+        /// </summary>
+        /// <param name="model">Geopoints object</param>
+        /// <returns>Returns ok if data send to geoService</returns>
         [AllowAnonymous]
         [HttpGet("getDistance")]
         public IActionResult GetDistance(GeoPoints model)
         {
             // publish message  
             _manager.Publish(model, "RabbitMQ", "fanout", "RabbitMQ");
-            return Ok("Message sent to endpoint");
+            return Ok("Distance inserted, you can check GetAllDistances");
         }
+
+        /// <summary>
+        /// Get all the distances for the given user
+        /// </summary>
+        /// <param name="user">user, always has only guid </param>
+        /// <returns>Returns list or not found.</returns>
         [AllowAnonymous]
         [HttpGet("GetAllDistances")]
-        public IActionResult GetAllDistances(string userGuid)
+        public IActionResult GetAllDistances(Entities.User user)
         {
             try
             {
                 Core.Data.DataProvider db = Core.Data.DataProvider.DataProviderFactory();
-                return Ok(db._data["GeoData"].Where(x => x.Key == System.Guid.Parse(userGuid)));
+                return Ok(db._data["GeoData"].Where(x => x.Key == System.Guid.Parse(user.GUID)));
             }
             catch (System.Exception)
             {
