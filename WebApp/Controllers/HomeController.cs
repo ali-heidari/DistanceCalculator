2e5af7ac8d8c8795b6c83f284eb85d1ce6f58805
ali <ali-heidari@outlook.com>
2020-03-10 11:38:40 +0330

    Unnecessary page removed

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -40,11 +40,6 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
