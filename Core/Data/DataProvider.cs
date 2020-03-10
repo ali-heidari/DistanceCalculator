e29fdf4f0236222c12550de6280743b364413cd9
ali <ali-heidari@outlook.com>
2020-03-10 21:41:53 +0330

    Comments updated

--- Core/Data/DataProvider.cs
+++ Core/Data/DataProvider.cs
@@ -6,6 +6,9 @@ using System.Linq;
 
 namespace Core.Data
 {
+    /// <summary>
+    /// Database layer
+    /// </summary>
     public class DataProvider
     {
         public Dictionary<string, Dictionary<Guid, string>> _data;
@@ -15,6 +18,10 @@ namespace Core.Data
             _data = new Dictionary<string, Dictionary<Guid, string>>();
         }
 
+        /// <summary>
+        /// Ensure there is only one object of database through app
+        /// </summary>
+        /// <returns>Returns DatabasProvider</returns>
         public static DataProvider DataProviderFactory()
         {
             if (_dp == null)
@@ -22,7 +29,11 @@ namespace Core.Data
             return _dp;
         }
 
-
+        /// <summary>
+        /// Insert into database
+        /// </summary>
+        /// <param name="obj">Dataobject to be inserted</param>
+        /// <returns>Returns true if successful otherwise false</returns>
         public bool Insert(DataObject obj)
         {
             try
@@ -43,6 +54,12 @@ namespace Core.Data
             }
         }
 
+        /// <summary>
+        /// Get user
+        /// </summary>
+        /// <param name="email">email of user</param>
+        /// <param name="password">password of user</param>
+        /// <returns>Returns user</returns>
         public User GetUser(string email, string password)
         {
             try
@@ -56,7 +73,7 @@ namespace Core.Data
                 User user = null;
                 foreach (var item in rows)
                 {
-                   user = JsonConvert.DeserializeObject<User>(item.Value);
+                    user = JsonConvert.DeserializeObject<User>(item.Value);
                     user.guid = item.Key;
                     if (user.email == email && user.password == password)
                         return user;
@@ -68,6 +85,13 @@ namespace Core.Data
                 return null;
             }
         }
+        /// <summary>
+        /// Get record by its guid
+        /// </summary>
+        /// <param name="docName">Name of document</param>
+        /// <param name="guid">the guid</param>
+        /// <typeparam name="T">Type of object which is inherited from DataObject</typeparam>
+        /// <returns>Returns object otherwise null</returns>
         public T GetData<T>(string docName, Guid guid) where T : DataObject
         {
 
