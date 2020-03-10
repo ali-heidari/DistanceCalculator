b1d0d260ed8250af422ecb65664f877296aaa4e5
ali <ali-heidari@outlook.com>
2020-03-10 13:22:35 +0330

    WebAPI initial commit

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebAPI.Services;
+using WebAPI.Models;
+using System.Linq;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _authService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _authService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ WebAPI/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace WebAPI.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
