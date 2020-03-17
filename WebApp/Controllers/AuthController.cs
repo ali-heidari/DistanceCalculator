29adce9791cd773ba37f1d1043eb0d8a59261d23
ali <ali-heidari@outlook.com>
2020-03-17 21:45:24 +0330

    Connect login controller to login api

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -28,7 +32,26 @@ namespace WebApp.Controllers
         {
             return View();
         }
-        
+
+        /// <summary>
+        /// Sends credentials to api for validation
+        /// </summary>
+        /// <param name="email">email</param>
+        /// <param name="password">password</param>
+        /// <returns>If OK redirects to dashboard otherwise show current view.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            RequestSender requestSender = new RequestSender();
+
+            var res = await requestSender.Post("auth/login", new { email, password });
+            if (res == HttpStatusCode.OK)
+            {
+                return RedirectToAction("Index", "Home");  
+            }
+
+            return View();
+        }
         /// <summary>
         /// Register action, shows the page to let user register new account
         /// </summary>
---
+++ WebApp/Helpers/RequestSender.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RequestSender
+{
+    private static HttpClient _client;
+
+    public RequestSender()
+    {
+        if (_client == null)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("http://localhost:4500/");
+        }
+    }
+
+    public async Task<HttpStatusCode> Post(string path, object content)
+    {
+        StringContent postContent = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync(path, postContent);
+        // var responseContent = await response.Content.ReadAsStringAsync();
+        return response.StatusCode;
+    }
+}
